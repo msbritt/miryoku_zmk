151842d4b7dcf864791419101470fe1e69ee7075
Matthew Britt <msbritt@gmail.com>
2023-09-03 21:52:34 -0400

    Add custom configs for nav and mac

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_NAV_INVERTEDT
+#define MIRYOKU_LAYERS_FLIP
+#define MIRYOKU_EXTRA_COLEMAKDH
+#define MIRYOKU_CLIPBOARD=MAC
